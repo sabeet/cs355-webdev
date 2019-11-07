40eb892913e8f61749554a6a40f536ce6368384d
Sabeet <sabeet.other.way@gmail.com>
2019-11-06 19:19:11 -0500

    Google search using CSE API now works.

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+function myFunction(){
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -6,12 +7,13 @@ xhttp.onreadystatechange = function() {
         //      
        var output = '';
 		for(var i=0; i< items.length; i++){
-		output += items[i].snippet + "<br>" + "<br>";
+		output += items[i].title + "<br>" + items[i].link + "<br>" + items[i].snippet + "<br>" + "<br>";
 	}
 	document.getElementById('smth').innerHTML = output;
 //
     }
-};
-    xhttp.open("GET", "https://www.googleapis.com/customsearch/v1?key=AIzaSyD06Z51s9ztRexJT5NIffyjapUON3Jj4hs&cx=005197151154116588693:rovpjytqdjf&q=sabeet", true);
+};  
+    var x = document.getElementById("search").value;
+    xhttp.open("GET", "https://www.googleapis.com/customsearch/v1?key=AIzaSyD06Z51s9ztRexJT5NIffyjapUON3Jj4hs&cx=005197151154116588693:rovpjytqdjf&q="+x, true);
     xhttp.send();
-
+}
